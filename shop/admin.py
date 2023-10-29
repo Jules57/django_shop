e34dcc043ac68d2a3ebc2c78736249a30e6a867f
Julia Chuprova <nikaniks57@gmail.com>
2023-10-29 22:47:37 +0200

    update models

--- shop/admin.py
+++ shop/admin.py
@@ -1,1 +1,32 @@
 from django.contrib import admin
+from .models import User, Product, Purchase, Return
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'first_name', 'last_name', 'wallet', 'image']
+    list_filter = ['username', 'wallet']
+    search_fields = ['username', 'last_name']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'price', 'quantity', 'image']
+    list_filter = ['title', 'price', 'quantity']
+    search_fields = ['title', 'price']
+
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'product_quantity', 'bought_at']
+    list_filter = ['user', 'product', 'bought_at']
+    search_fields = ['product', 'user']
+    date_hierarchy = 'bought_at'
+
+
+@admin.register(Return)
+class ReturnAdmin(admin.ModelAdmin):
+    list_display = ['purchase', 'returned_at']
+    list_filter = ['purchase', 'returned_at']
+    search_fields = ['purchase', 'returned_at']
+    date_hierarchy = 'returned_at'
--- shop/models.py
+++ shop/models.py
@@ -1,1 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
+
+
+class User(AbstractUser):
+    wallet = models.DecimalField(decimal_places=2, max_digits=8, default=10000)
+    image = models.ImageField(upload_to='shop/static/shop/images/', max_length=100, null=True)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='shop/static/shop/images/', max_length=100, null=True, blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                       args=[self.id])
+
+
+class Purchase(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='purchases')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='purchased')
+    product_quantity = models.PositiveIntegerField()
+    bought_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.product} bought by {self.user} at {self.purchase_time}'
+
+
+class Return(models.Model):
+    purchase = models.OneToOneField(Purchase, on_delete=models.CASCADE, related_name='returned')
+    returned_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.purchase.product.title} returned by {self.purchase.user}'
