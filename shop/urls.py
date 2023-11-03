24cec38d8ede4b710d674db092d90daa78f7aa6b
Julia Chuprova <nikaniks57@gmail.com>
2023-11-03 14:34:19 +0200

    add purchase list

--- shop/urls.py
+++ shop/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from shop.views import Login, UserRegisterView, ProductListView, ProductDetailView, ProductCreateView, \
-    ProductUpdateView, ProductDeleteView, PurchaseCreateView
+    ProductUpdateView, ProductDeleteView, PurchaseCreateView, PurchaseListView
 
 app_name = 'shop'
 
@@ -13,6 +13,7 @@ urlpatterns = [
     path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='update_product'),
     path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='delete_product'),
     path('purchase/<int:pk>/', PurchaseCreateView.as_view(), name='purchase'),
+    path('purchases/', PurchaseListView.as_view(), name='purchase_list'),
     path('login/', Login.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('register/', UserRegisterView.as_view(), name='register'),
